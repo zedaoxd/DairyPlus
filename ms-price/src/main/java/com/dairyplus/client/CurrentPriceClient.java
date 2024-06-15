1e4b76b232e6aac05aed65c5291971a1ffae8ed0
Bruno Lessa <Brunolessa@live.com>
2024-06-15 03:46:15 -0300

    feat: create CurrentPriceClient

---
+++ ms-price/src/main/java/com/dairyplus/client/CurrentPriceClient.java
@@ -0,0 +1,18 @@
+package com.dairyplus.client;
+
+import com.dairyplus.dto.CurrentPriceDTO;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/last")
+@RegisterRestClient(baseUri="https://economia.awesomeapi.com.br")
+@ApplicationScoped
+public interface CurrentPriceClient {
+
+    @GET
+    @Path("/{pair}")
+    CurrentPriceDTO getCurrentPrice(@PathParam("pair") String pair);
+}
