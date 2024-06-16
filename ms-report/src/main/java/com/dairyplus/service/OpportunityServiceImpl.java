0f8d3a5f3417772d036d0192cb9324061f214ec3
Bruno Lessa <Brunolessa@live.com>
2024-06-16 17:54:57 -0300

    feat: create OpportunityService

---
+++ ms-report/src/main/java/com/dairyplus/service/OpportunityServiceImpl.java
@@ -0,0 +1,63 @@
+package com.dairyplus.service;
+
+import com.dairyplus.dto.OpportunityDTO;
+import com.dairyplus.dto.ProposalDTO;
+import com.dairyplus.dto.QuotatioDTO;
+import com.dairyplus.entity.OpportunityEntity;
+import com.dairyplus.entity.QuotationEntity;
+import com.dairyplus.repository.OpportunityRepository;
+import com.dairyplus.repository.QuotationRepository;
+import com.dairyplus.utils.CSVHelper;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+@ApplicationScoped
+public class OpportunityServiceImpl implements OpportunityService {
+    @Inject
+    QuotationRepository quotationRepository;
+
+    @Inject
+    OpportunityRepository opportunityRepository;
+
+    @Override
+    @Transactional
+    public void buildOpportunity(ProposalDTO dto) {
+        var quotation = quotationRepository.findLatestQuotation();
+
+        var opportunity = new OpportunityEntity();
+        opportunity.setProposalId(dto.id());
+        opportunity.setCustomer(dto.customer());
+        opportunity.setGallonPrice(dto.gallonPrice());
+        opportunity.setCurrentPrice(quotation.getCurrentPrice());
+
+        opportunityRepository.persist(opportunity);
+    }
+
+    @Override
+    @Transactional
+    public void saveQuotation(QuotatioDTO dto) {
+        var quotation = new QuotationEntity();
+        quotation.setCurrentPrice(dto.currentPrice());
+        quotationRepository.persist(quotation);
+    }
+
+    @Override
+    public List<OpportunityDTO> generateOpportunity() {
+        return null;
+    }
+
+    @Override
+    public ByteArrayInputStream generateCSVOpportunity() {
+        var opportunitiesDTO = opportunityRepository
+                .listAll()
+                .stream()
+                .map(OpportunityEntity::toDTO)
+                .toList();
+
+        return CSVHelper.opportunitiesToCSV(opportunitiesDTO);
+    }
+}
